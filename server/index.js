51645727f143398c2b7106d93b66ae838fc11cea
Nikintelligence <bna20020209@gmail.com>
2022-03-28 21:05:38 +0300

    creating bot

--- server/index.js
+++ server/index.js
@@ -3,18 +3,23 @@ const TelegramApi = require('node-telegram-bot-api')
 
 const bot = new TelegramApi(process.env.BOT_TOKEN, {polling: true});  //токен из .env
 
+bot.setMyCommands([
+    {command: '/start', description: 'Приветствие'},
+    {command: '/info', description: 'Информация о боте'},
+])
+
 bot.on('message', msg => {
     // console.log(msg)
     const text = msg.text;
     const chatId = msg.chat.id;
 
     if (text === '/start') {
-        bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/667/076/66707684-3a82-396b-aadd-1b2c6d3e4a5c/5.webp')
-        bot.sendMessage(chatId,
-            `Добро пожаловать в Telegram бот "Консультант преподавателя Политеха" Вы можете ознаомиться с функциями бота, нажав на /info`)
+        bot.sendPhoto(chatId, 'src/images/ioysticer.png')
+        bot.sendMessage(chatId, `Добро пожаловать в Telegram бот "Консультант преподавателя Политеха" Вы можете ознаомиться с функциями бота, нажав на /info`)
     }
 
     if (text === '/info') {
+        bot.sendPhoto(chatId, 'src/images/holdcoffe.jpg')
         bot.sendMessage(chatId, `${msg.from.first_name}, на данный момент бот находится на стадии разработки`)
     }
 })
